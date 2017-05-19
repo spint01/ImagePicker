f5b0320f9b0584769cb531e5cf88175209e6ad67
Steve Pint <stevepint@Steve-Pints-MacBook-Pro-3.local>
2017-05-18 23:59:42 -0500

    Improve Example

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -61,12 +61,42 @@ extension ViewController: ImagePickerDelegate {
   func photoAdded(_ imagePicker: ImagePickerController, photo: PHAsset) {
     print("added photo")
     DispatchQueue.main.async {
-      let alert = UIAlertController(title: "Added Photo", message: photo.description, preferredStyle: .alert)
-      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-
-      alert.view.transform = Helper.rotationTransform()
-      imagePicker.present(alert, animated: false, completion: nil)
+//      let alert = UIAlertController(title: "Added Photo", message: photo.description, preferredStyle: .alert)
+//      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//
+//      alert.view.transform = Helper.rotationTransform()
+//      imagePicker.present(alert, animated: false, completion: nil)
     }
+    photo.requestMetadata(withCompletionBlock: { (info) in
+      print("photo metadata: \(info)")
+      if let gpsInfo = info["{GPS}"] as? [String: Any] {
+        print("photo {GPS} metadata: \(gpsInfo)")
+        if let direction = gpsInfo["ImgDirection"] {
+          print("photo {GPS} direction metadata: \(direction)")
+        }
+      }
+    })
   }
 }
+
+public extension PHAsset {
+
+  public func requestMetadata(withCompletionBlock completionBlock: @escaping (([String: Any]) -> Void)) {
+    DispatchQueue.global(qos: .default).async(execute: {() -> Void in
+      let editOptions = PHContentEditingInputRequestOptions()
+      editOptions.isNetworkAccessAllowed = true
+      self.requestContentEditingInput(with: editOptions, completionHandler: { (contentEditingInput, info) -> Void in
+        if let input = contentEditingInput, let url = input.fullSizeImageURL {
+          let image = CIImage(contentsOf: url)
+          DispatchQueue.main.async(execute: {() -> Void in
+            if let properties = image?.properties {
+              completionBlock(properties)
+            }
+          })
+        }
+      })
+    })
+  }
+}
+
 
